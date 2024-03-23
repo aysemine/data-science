864952d5ba93a538f1a7bc1db84eec65ea0adc6a
aysemine <ayseminerolur@gmail.com>
2024-03-23 11:41:15 +0300

    data types in python

---
+++ data_types.py
@@ -0,0 +1,21 @@
+#define integer variable and check the type
+x = 46
+print(type (x))
+
+#convert integer to float
+print(type(float(x)))
+
+#string
+string = "hello"
+print(type(string))
+
+#find spesific index in string
+print(string[0])
+
+#multiple index
+print(string[0:2])
+
+#check for characters in string
+print("hell" in string)
+
+
